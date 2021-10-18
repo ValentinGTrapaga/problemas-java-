58b1ffd405da921937830c2413838fb2df4e245c
ValentinGTrapaga <valentingt22@gmail.com>
2021-10-18 02:49:11 -0300

    Termine guia 1 de los problemas de Java

--- scripts.js
+++ scripts.js
@@ -17,4 +17,46 @@ if (num1 > num2) {
     alert("Los numeros son iguales");
 } else {
     alert("El segundo numero es mayor");
+} // P6
+
+if (num1%2 === 0) {
+    alert(`El numero ${Number(num1)} es divisible por 2`);
+} else {
+    alert(`El numero ${Number(num1)} no es divisible por 2`)
+} // P8
+
+let frase = prompt("Por favor, inserte una frase para analizar la aparicion de vocales");
+let numA = 0,
+    numE = 0,
+    numI = 0,
+    numO = 0,
+    numU = 0;
+
+let largoFrase = frase.length;
+for(let i = 0; i > largoFrase; i++) {
+    switch(frase[i]) {
+        case 'a':
+        case 'A':
+            numA++;
+            break;
+        case 'e':
+        case 'E':
+            numE++;
+            break;
+        case 'i':
+        case 'I':
+            numI++;
+            break;
+        case 'o':
+        case 'O':
+            numO++;
+            break;
+        case 'u':
+        case 'U':
+            numU++;
+            break;
+    }
 }
+
+alert(`La cantidad de veces que ha aparecido cada letra en la frase es:
+    A: ${numA}, E: ${numE}, I: ${numI}, O: ${numO}, U: ${numU}.`) // P12 DEBUGGEAR!!!
