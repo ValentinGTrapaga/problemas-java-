df04bc2df3c2fa30c5eb1e1492436a15fa33abc2
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-04-21 03:03:16 -0300

    ui finished, needs some styling

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,6 +1,6 @@
 let computerChoices = ['rock', 'paper', 'scissors'];
-let playerScore = 0;
-let compScore = 0;
+var playerScore = 0;
+var compScore = 0;
 let playerChoice;
 
 function computerPlay () {
@@ -8,7 +8,7 @@ function computerPlay () {
     return result.toLowerCase();
   }
 
-function eligeJugador() {
+/* function eligeJugador() {
     playerChoice = prompt("Please choose: Rock, Paper, Scissors")
     playerChoice = playerChoice.toLowerCase()
     if(computerChoices.includes(playerChoice)) {
@@ -17,23 +17,47 @@ function eligeJugador() {
     else {
         alert("Error, must choose only between Rock, Paper, Scissors. You lost this chance!")
     }
-}
+} */
+
+const score = document.getElementById("score")
+const round = document.getElementById("round")
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice == computerChoice.toLowerCase()) {
-        console.log("It's a draw")
+        round.innerText = `It's a draw`
+        score.innerText = `Result is Player: ${playerScore}, Computer: ${compScore}`
     }
     if ((playerChoice == "rock" && computerChoice.toLowerCase() == "scissors") ||
     (playerChoice == "paper" && computerChoice.toLowerCase() == "rock")) {
         playerScore++
-        console.log(`You win, ${playerChoice} beats ${computerChoice}`)
+        round.innerText = `You win, ${playerChoice} beats ${computerChoice}`
+        score.innerText = `Result is Player: ${playerScore}, Computer: ${compScore}`
     }
     if ((computerChoice.toLowerCase() == "rock" && playerChoice == "scissors") ||
     (computerChoice.toLowerCase() == "paper" && playerChoice == "rock")) {
         compScore++
-        console.log(`You lose, ${computerChoice} beats ${playerChoice}`)
+        round.innerText = `You lose, ${computerChoice} beats ${playerChoice}`
+        score.innerText = `Result is Player: ${playerScore}, Computer: ${compScore}`
     }
 }
+
+const btns = document.querySelectorAll('.boton')
+
+for (i of btns) {
+    i.addEventListener('click', (e) => {
+        computerChoice = computerPlay()
+        playerChoice = e.target.id
+        playRound(playerChoice, computerChoice)
+        if ((playerScore === 5) || (compScore === 5)) {
+            btns.forEach((e) => e.className = "boton col btn btn-dark mx-2 disabled")
+            score.innerText = `Game over! It was the first to score 5 points.
+            Result is Player: ${playerScore}, Computer: ${compScore}`
+            round.style.display = "none"
+        }
+    })
+}
+
+
 
 /* function game() {
     for (let i = 0; i < 5; i++) {
